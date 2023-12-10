73b953771c378ba060b41fe94e9fd049e304f433
Sliderer <slidedr8ugs@mail.ru>
2023-12-10 21:50:18 +0300

    feat back: add Spring project

---
+++ CMonitor/src/main/java/com/example/cmonitor/CMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.example.cmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CMonitorApplication.class, args);
+    }
+
+}
---
+++ CMonitor/src/test/java/com/example/cmonitor/CMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
